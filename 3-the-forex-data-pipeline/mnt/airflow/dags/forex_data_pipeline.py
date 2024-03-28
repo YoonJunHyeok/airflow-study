ef6f444b8eada7fb33259a03971f6964ce6b9d10
YoonJunHyeok <jhyoon1607@gmail.com>
2024-03-28 19:51:56 +0900

    feat: section #3 ing

---
+++ 3-the-forex-data-pipeline/mnt/airflow/dags/forex_data_pipeline.py
@@ -0,0 +1,88 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.sensors.filesystem import FileSensor
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
+from datetime import datetime, timedelta
+
+import csv
+import json
+import requests
+
+default_args = {
+    "owner": "airflow",
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "email": "admin@localhost.com",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5)
+}
+
+'''
+Download forex rates according to the currencies we want to watch
+described in the file forex_currencies.csv
+'''
+@task(task_id="download_rates")
+def download_rates():
+    BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
+    ENDPOINTS = {
+        'USD': 'api_forex_exchange_usd.json',
+        'EUR': 'api_forex_exchange_eur.json'
+    }
+    with open('/opt/airflow/dags/files/forex_currencies.csv') as forex_currencies:
+        reader = csv.DictReader(forex_currencies, delimiter=';')
+        for idx, row in enumerate(reader):
+            base = row['base']
+            with_pairs = row['with_pairs'].split(' ')
+            indata = requests.get(f"{BASE_URL}{ENDPOINTS[base]}").json()
+            outdata = {'base': base, 'rates': {}, 'last_update': indata['date']}
+            for pair in with_pairs:
+                outdata['rates'][pair] = indata['rates'][pair]
+            with open('/opt/airflow/dags/files/forex_rates.json', 'a') as outfile:
+                json.dump(outdata, outfile)
+                outfile.write('\n')
+
+with DAG(
+    "forex_data_pipeline", 
+    start_date=datetime(2024, 1 ,1), 
+    schedule_interval="@daily", 
+    catchup=False,
+    default_args=default_args, 
+) as dag:
+    # check availability of forex rates
+    is_forex_rates_available = HttpSensor(
+        task_id="is_forex_rates_available",
+        http_conn_id="forex_api",
+        endpoint="marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b",
+        response_check=lambda response: "rates" in response.text,
+        poke_interval=5,
+        timeout=20
+    )
+
+    # check availability of the file having currencies to watch
+    is_forex_currencies_file_available = FileSensor(
+        task_id="is_forex_currencies_file_available",
+        fs_conn_id="forex_path",
+        filepath="forex_currencies.csv",
+        poke_interval=5,
+        timeout=20
+    )
+
+    # donwload forex rates with python to local file system
+    # download_rates = PythonOperator(
+    #     task_id="download_rates",
+    #     python_callable=download_rates
+    # )
+    # without @task decorator
+    download_rates = download_rates()
+
+    # save the forex rates in HDFS
+    save_rates = BashOperator(
+        task_id="save_rates",
+        bash_command="""
+            hdfs dfs -mkdir -p /forex && \
+            hdfs dfs -put -f $AIRFLOW_HOME/dags/files/forex_rates.json /forex
+        """
+    )
---
+++ 3-the-forex-data-pipeline/readme.md
@@ -0,0 +1,53 @@
+# Usage Guide
+
+To Start and run Airflow: start.sh
+
+To Restart Airflow: restart.sh
+
+To Stop Airflow: stop.sh
+
+To remove everything(metadata): reset.sh
+
+## Docker Reminder - Use Docker Compose for multi-container.
+
+1. Docker file
+2. Docker image
+3. Docker Compose file(yaml file) descibing the services that we wnat to run for our app. (ex. webserver, scheduler, database)
+   / All services are single application / All Containers will share the same network. So, they are able to communicate with each other.
+
+## Flow Chart of the Forex Data Pipeline
+
+1. Check availability of forex rates (V)
+2. Check availability of the file having currencies to watch
+3. Download forex rates with Python
+4. Save the forex rates in HDFS(Hadoop Distributed File System)
+5. Create a Hive table to store forex rates from the HDFS
+6. Process forex rates with Spark
+7. Send and Email Notification
+8. Send a Slack Notification
+
+## Operators
+
+1. Action Operator: to execute something. ex. python operator, bash operator
+2. Transfer Operator: to transfer data from a source to a destination. ex. presto to mysql operator, mysql to gcs operator
+3. Censor Operator: to wait for something to happen before moving to the next tasks / For example, we want to wait for a file to land at a specific location in our file system => file censor operator
+
+## Additional Things
+
+```bash
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+```
+
+- Apple 칩 이슈로 위의 명령어를 실행해야 한다.
+- 클라우드 서버에 맞춰서 이미지를 빌드한 것이기에 로컬에서는 실행되지 않는다.
+
+```bash
+docker exec -it ${container_id} /bin/dash
+```
+
+run bash session in airflow container
+"before running DAG, we should test task"
+
+```bash
+airflow tasks test ${dag_id} ${task_id} ${execution_date}
+```
